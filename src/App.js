00bbb0087cf791098d1cae2addd6f1e2f755aa7c
aLe3ouLa <ale3mp90@gmail.com>
2020-04-08 20:57:30 +0200

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Home from "./pages/homepage/home";
 
 function App() {
-  return <div>Hello World</div>;
+  return <Home />;
 }
 
 export default App;
---
+++ src/styles/variables/colors.js
@@ -0,0 +1,14 @@
+export const color = {
+  primary: "#44AC8B",
+  secondary: "#FF9142",
+  tertiary: "#2B42A1",
+
+  text: "#4F4E56",
+  textLight: "#757576",
+
+  white: "#ffffff",
+  black: "#000000",
+
+  warning: "#efb320",
+  error: "#c82c1a",
+};
