d3606822db8ec7b31ed524187a91cfe0a1f968f2
aLe3ouLa <ale3mp90@gmail.com>
2020-04-09 08:42:38 +0200

    add styling into navigation

--- src/components/layout/topbar/index.js
+++ src/components/layout/topbar/index.js
@@ -1,20 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 5rem;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  padding: 0.5rem 2rem;
+`;
+
+const NavigationList = styled.ul`
+  display: flex;
+  & > li:not(:last-child) {
+    margin-right: ${({ theme }) => theme.spacing.sm};
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+`;
 
 const Topbar = () => {
   return (
-    <header>
-      <h1>Corona Tracker</h1>
+    <Header>
+      <Title>Corona Tracker</Title>
 
       <nav>
-        <ul>
+        <NavigationList>
           <li>Travel Alert</li>
           <li>What is COVID-19</li>
           <li>Prevention</li>
           <li>Analytics</li>
           <li>About</li>
-        </ul>
+        </NavigationList>
       </nav>
-    </header>
+    </Header>
   );
 };
 
